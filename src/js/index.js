5a9f04d2ec17b2bc7d0e151f09419f0630a21df6
simtaxx <souici.haris@gmail.com>
2019-05-18 19:52:22 +0200

    contact end

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,12 @@
-import { copyFile } from "fs";
-
 let contactButton = document.querySelector(".contact");
 let body = document.querySelector(".projects");
 let body2 = document.querySelector("main");
-console.log(body);
 contactButton.addEventListener("click", () => {
-  contactButton.classList.add("open");
+  contactButton.classList.toggle("open");
 });
 body.addEventListener("click", () => {
-  console.log("lourd");
   contactButton.classList.remove("open");
 });
 body2.addEventListener("click", () => {
-  console.log("lourd");
   contactButton.classList.remove("open");
 });
